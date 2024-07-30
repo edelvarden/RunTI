d8732aba36dfaf58df46fb9f6d8d447a08f08c88
edelvarden <edelvarden@gmail.com>
2024-07-30 01:49:56 -0700

    fixes: elevate privilages to destroy protected files

--- src/RunTI/Program.cs
+++ src/RunTI/Program.cs
@@ -196,8 +196,22 @@ namespace RunTI
         {
             string exe = "powershell.exe";
             string arguments = "-NoProfile -NoLogo -ExecutionPolicy Unrestricted";
+            string formattedPath = path.Trim().TrimStart('"').TrimEnd('"');
+            string formattedCommandPath = @"\" + $"\"{formattedPath}" + @"\" + "\""; // format path to \"path\"
 
-            string deleteCommand = $"-Command \"Remove-Item -Path " + @"\" + $"\"{path.Trim().TrimStart('"').TrimEnd('"')}" + @"\" + "\""; // format path to \"path\"
+            string admins = @"\" + $"\"Administrators:(F)" + @"\" + "\""; // \"Administrators:(F)\"
+            string users = @"\" + $"\"Users:(F)" + @"\" + "\""; // \"Users:(F)\"
+            string system = @"\" + $"\"SYSTEM:(F)" + @"\" + "\""; // \"SYSTEM:(F)\"
+            string trustedinstaller = @"\" + $"\"NT SERVICE\\TrustedInstaller:(F)" + @"\" + "\""; // \"NT SERVICE\TrustedInstaller:(F)\"
+
+            string elevatePrivilagesCommand = $"takeown /f {formattedCommandPath}; icacls {formattedCommandPath} /grant {system} /t; icacls {formattedCommandPath} /grant {admins} /t; icacls {formattedCommandPath} /grant {users} /t";
+
+            if (IsDirectory($"{formattedPath}"))
+            {
+                elevatePrivilagesCommand = $"takeown /f {formattedCommandPath} /r /d y; icacls {formattedCommandPath} /grant {system} /t; icacls {formattedCommandPath} /grant {admins} /t; icacls {formattedCommandPath} /grant {users} /t";
+            }
+
+            string deleteCommand = $"-Command \"{elevatePrivilagesCommand}; Remove-Item -Path " + formattedCommandPath;
 
             deleteCommand += " -Recurse -Force -ErrorAction SilentlyContinue\"";
 
@@ -210,6 +224,16 @@ namespace RunTI
             }
         }
 
+        public static bool IsDirectory(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                throw new ArgumentException("Path cannot be null or empty.", nameof(path));
+            }
+
+            FileAttributes attributes = File.GetAttributes(path);
+            return (attributes & FileAttributes.Directory) == FileAttributes.Directory;
+        }
 
         static void ParseCmdLine(string[] args)
         {
